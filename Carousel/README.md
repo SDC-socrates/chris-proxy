53610531f25c303623f0ee6f4e7e420612f2af47
ccades <hellocaden@gmail.com>
2019-01-25 15:52:27 -0800

    working aws proxy

--- Carousel/README.md
+++
@@ -1,7 +0,0 @@
-# Kyle--Carousel
-
-# To seed database
-
-https://www.npmjs.com/package/node-mongo-seeds
-
-from the command line run seed
--- Carousel/client/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Carousel from './components/Carousel';
-
-ReactDOM.render(<Carousel id={12} />, document.getElementById('Carousel'));
